05caba16f3c2a5a84b5cbd847deadb8c71073589
k1m2njun <k1m2njun@naver.com>
2023-07-11 01:53:07 +0900

    Feat: [#5] 추가 AdminApplication.java
    관리자용 어플리케이션을 port를 달리해 추가합니다.
    관리자 어플리케이션은 MyBatis를 기반으로 합니다.

---
+++ src/main/java/com/adminapplication/AdminApplication.java
@@ -0,0 +1,11 @@
+package com.adminapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.adminapplication.AdminApplication.class, args);
+    }
+}
