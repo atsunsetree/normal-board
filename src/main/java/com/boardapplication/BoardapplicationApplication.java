d177247483f5c60ea6ee63bc47af24509bcebdc9
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-10 16:42:58 +0900

    Init: 프로젝트 세팅

---
+++ src/main/java/com/boardapplication/BoardapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.boardapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boardapplication/BoardapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
