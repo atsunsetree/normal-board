87b7203fa3fe4892d7684f8f6fbcdfdb5365804f
k1m2njun <k1m2njun@naver.com>
2023-07-11 14:11:10 +0900

    Chor: 추가 board dependencies
    admin 어플리케이션에서 board 모듈의 클래스에 접근할 수 있도록 의존성 주입

--- module-admin/src/main/java/com/adminapplication/admin/AdminRepository.java
+++ module-admin/src/main/java/com/adminapplication/admin/AdminRepository.java
@@ -1,4 +1,11 @@
 package com.adminapplication.admin;
 
+import com.boardapplication.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface AdminRepository {
+    List<User> findAllUsers();
 }
