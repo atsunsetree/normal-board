ae2cd04b1778a50d8d5d73786cc3dd7b96a0c53e
k1m2njun <k1m2njun@naver.com>
2023-07-12 18:34:48 +0900

    Feat: [#1] 추가 findAllUsersInfo()
    관리자 메인 페이지에 회원정보(닉네임, 이메일, 권한, 게시글 수, 댓글 수)를 화면에 출력합니다.
    MyBatis - Tymeleaf - MVC

--- module-admin/src/main/java/com/adminapplication/admin/AdminService.java
+++ module-admin/src/main/java/com/adminapplication/admin/AdminService.java
@@ -1,5 +1,6 @@
 package com.adminapplication.admin;
 
+import com.adminapplication.dto.AllUsersInfoResponseDto;
 import com.core.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,13 +8,20 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AdminService {
+public class AdminService { // 비즈니스 로직
 
     @Autowired
     private AdminRepository adminRepository;
 
     public List<User> getUserList() {
         return adminRepository.findAllUsers();
+    }
+
+    public List<AllUsersInfoResponseDto> getUserInfoList() {
+        List<AllUsersInfoResponseDto> allUsersInfoResponseDto = adminRepository.findAllUsersInfo();
+        System.out.println(allUsersInfoResponseDto.get(0).toString());
+
+        return adminRepository.findAllUsersInfo();
     }
 
 }
