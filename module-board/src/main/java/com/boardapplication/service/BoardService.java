0758dbb14e4051542907fea25ca75b43a070be4e
juhee <a07224@naver.com>
2023-07-14 15:18:52 +0900

    Feat: [#21] 추가 Status가 NORMAL일 경우에만 게시글 출력
    사용자의 Status가 Normal일 경우만 게시글이 보이게 한다

--- module-board/src/main/java/com/boardapplication/service/BoardService.java
+++ module-board/src/main/java/com/boardapplication/service/BoardService.java
@@ -3,6 +3,7 @@ package com.boardapplication.service;
 import com.boardapplication.dto.BoardDto;
 import com.boardapplication.repository.BoardRepository;
 import com.core.entity.Board;
+import com.core.entity.Status;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,19 +22,20 @@ public class BoardService {
     public List<BoardDto> getBoardList(){
         List<Board> boards = boardRepository.findAll();
         List<BoardDto> boardDtoList = new ArrayList<>();
-
         for(Board board : boards){
-            BoardDto dto = BoardDto.builder()
-                    .id(board.getId())
-                    .userId(board.getId())
-                    .title(board.getTitle())
-                    .content(board.getContent())
-                    .thumbnail(board.getThumbnail())
-                    .status(board.getStatus())
-                    .createdAt(board.getCreatedAt())
-                    .updatedAt(board.getUpdatedAt())
-                    .build();
-            boardDtoList.add(dto);
+            if(board.getStatus().equals(Status.NORMAL)){
+                BoardDto dto = BoardDto.builder()
+                        .id(board.getId())
+                        .userId(board.getId())
+                        .title(board.getTitle())
+                        .content(board.getContent())
+                        .thumbnail(board.getThumbnail())
+                        .status(board.getStatus())
+                        .createdAt(board.getCreatedAt())
+                        .updatedAt(board.getUpdatedAt())
+                        .build();
+                boardDtoList.add(dto);
+            }
         }
         return boardDtoList;
     }
