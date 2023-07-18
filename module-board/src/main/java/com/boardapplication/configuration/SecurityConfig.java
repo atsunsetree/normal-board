d5c1da17eee7182b81e1bce133c6aae7f90f13df
atsunsetree <atsunsetree529@gmail.com>
2023-07-18 12:52:59 +0900

    Feat: [#8] 패스워드 인코더 빈 생성

--- module-board/src/main/java/com/boardapplication/configuration/SecurityConfig.java
+++ module-board/src/main/java/com/boardapplication/configuration/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.boardapplication.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,9 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .mvcMatchers("/","/test","/login","/join").permitAll()
+                .mvcMatchers("/","/test","/login","/join", "/profile").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/user/*").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
 
