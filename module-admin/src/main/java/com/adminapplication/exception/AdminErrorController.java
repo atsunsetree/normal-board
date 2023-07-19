54aaa975b5040d6055be073ba1858d6b49f8c959
k1m2njun <k1m2njun@naver.com>
2023-07-19 21:59:49 +0900

    Feat: [#40] 추가 커스텀에러페이지

---
+++ module-admin/src/main/java/com/adminapplication/exception/AdminErrorController.java
@@ -0,0 +1,14 @@
+package com.adminapplication.exception;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "/error";
+    }
+}
