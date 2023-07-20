ffc3069ec42683e1ac244cc5b93e370d2b52dfcc
atsunsetree <atsunsetree529@gmail.com>
2023-07-20 10:37:55 +0900

    Feat:[#8] 로그인 구현

--- module-board/src/main/java/com/boardapplication/dto/JoinDTO.java
+++ module-board/src/main/java/com/boardapplication/dto/JoinDTO.java
@@ -1,20 +1,17 @@
 package com.boardapplication.dto;
 
 import com.core.entity.User;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.core.entity.Role;
 
-import javax.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
 public class JoinDTO {
-
     @NotEmpty
     @Length(min = 3, max = 20)
     @Pattern(regexp = "^[a-zA-Z0-9]{3,20}$")
@@ -28,9 +25,24 @@ public class JoinDTO {
     @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")
     private String email;
 
-    @NotNull
+    @NotEmpty
     @Length(min = 3, max = 20)
     @Pattern(regexp = "^[a-zA-Z가-힣]{3,20}$")
     private String nickname;
+
+    public User toEntity(PasswordEncoder passwordEncoder) {
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setNickname(nickname);
+        user.setRole(Role.NORMAL);
+
+        if (password != null) {
+            String enPassword = passwordEncoder.encode(password);
+            user.setPassword(enPassword);
+        }
+
+        return user;
+    }
 
 }
