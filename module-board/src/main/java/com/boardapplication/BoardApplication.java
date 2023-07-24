f14961b288499c576b2abaa6143b5d4966a29cee
atsunsetree <atsunsetree529@gmail.com>
2023-07-24 14:06:53 +0900

    Feat:[#47] 회원정보, 수정 구현

--- module-board/src/main/java/com/boardapplication/BoardApplication.java
+++ module-board/src/main/java/com/boardapplication/BoardApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EntityScan(basePackageClasses = {EntityBasePackage.class, BoardBasePackage.class})
+@EnableJpaAuditing
 public class BoardApplication {
 
     public static void main(String[] args) {
--- module-core/src/main/java/com/core/entity/User.java
+++ module-core/src/main/java/com/core/entity/User.java
@@ -3,6 +3,7 @@ package com.core.entity;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,7 +12,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-//@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener.class)
 @Entity(name = "user_tb")
 @Builder
 public class User {
@@ -19,28 +20,28 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Column(unique = true, nullable = false, length = 20)
+    @Column(unique = true, nullable = false, length = 20)
     private String username;
 
-//    @Column(nullable = false, length = 60) // 패스워드 인코딩(BCrypt)
+    @Column(nullable = false, length = 60) // 패스워드 인코딩(BCrypt)
     private String password;
 
-//    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 20)
     private String email;
 
     @Column(nullable = false, length = 20)
     private String nickname;
 
     @Enumerated(EnumType.STRING)
-//    @Column(nullable = false)
+    @Column(nullable = false)
     private Role role; // NORMAL, VIP, BLACK
 
     @CreatedDate
-//    @Column(nullable = false)
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-//    @Column(nullable = false)
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
 }
